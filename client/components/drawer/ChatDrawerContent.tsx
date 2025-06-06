78f37bb8af752232b5dbd12ef1ddc5948bc8766b
kristjanpeterson <kristjan@nftport.xyz>
2025-06-06 10:58:55 +0300

    Sidebar improvements WIP

--- client/components/drawer/ChatDrawerContent.tsx
+++ client/components/drawer/ChatDrawerContent.tsx
@@ -1,10 +1,9 @@
-import { DrawerContentComponentProps, DrawerContentScrollView } from '@react-navigation/drawer'
-import { Pressable } from 'react-native'
-import { useChat } from '@/context/ChatContext'
-import { useRouter } from 'expo-router'
-import { ThemedText } from '@/components/theme/ThemedText'
-import { ThemedView } from '@/components/theme/ThemedView'
-import { Chat } from '@/types/chat'
+import {DrawerContentComponentProps, DrawerContentScrollView} from '@react-navigation/drawer'
+import {Pressable, View} from 'react-native'
+import {useChat} from '@/context/ChatContext'
+import {useRouter} from 'expo-router'
+import {ThemedText} from '@/components/theme/ThemedText'
+import {Chat} from '@/types/chat'
 
 export default function ChatDrawerContent(props: DrawerContentComponentProps) {
   const { chats, setSelectedChat, setActiveChat } = useChat()
@@ -18,18 +17,18 @@ export default function ChatDrawerContent(props: DrawerContentComponentProps) {
 
   return (
     <DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1 }}>
-      <ThemedView className="h-full flex-1 justify-between">
-        <ThemedView className="p-4">
+      <View className="h-full flex-1 justify-between">
+        <View className="p-4">
           {chats.map(chat => (
             <Pressable key={chat.id} onPress={() => onSelectChat(chat)} className="mb-2">
               <ThemedText className="text-base">{chat.title ?? 'Untitled Chat'}</ThemedText>
             </Pressable>
           ))}
-        </ThemedView>
-        <ThemedView className="w-full p-4">
+        </View>
+        <View className="w-full p-4">
           <ThemedText>User Name</ThemedText>
-        </ThemedView>
-      </ThemedView>
+        </View>
+      </View>
     </DrawerContentScrollView>
   )
 }
